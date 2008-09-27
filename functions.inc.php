8baed44d053df4045aa8ca215a61c64b54b69f39
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2008-09-27 02:15:37 +0000

    fixes #3240 reset cdr after authenticating pins on outbound calls so that the CDR reflects the proper billsec and disposition of the real call

--- functions.inc.php
+++ functions.inc.php
@@ -50,8 +50,10 @@ function pinsets_get_config($engine) {
 				// write out a macro that handles the authenticate
 				$ext->add('macro-pinsets', 's', '', new ext_gotoif('${ARG2} = 1','cdr,1'));
 				$ext->add('macro-pinsets', 's', '', new ext_execif('$["${DB(AMPUSER/${AMPUSER}/pinless)}" != "NOPASSWD"]', 'Authenticate',$asterisk_conf['astetcdir'].'/pinset_${ARG1}'));
+				$ext->add('macro-pinsets', 's', '', new ext_execif('$["${DB(AMPUSER/${AMPUSER}/pinless)}" != "NOPASSWD"]', 'ResetCDR'));
 				// authenticate with the CDR option (a)
 				$ext->add('macro-pinsets', 'cdr', '', new ext_execif('$["${DB(AMPUSER/${AMPUSER}/pinless)}" != "NOPASSWD"]', 'Authenticate',$asterisk_conf['astetcdir'].'/pinset_${ARG1},a'));
+				$ext->add('macro-pinsets', 'cdr', '', new ext_execif('$["${DB(AMPUSER/${AMPUSER}/pinless)}" != "NOPASSWD"]', 'ResetCDR'));
 			}
 		break;
 	}
