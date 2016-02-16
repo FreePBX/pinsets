051450319dc6c14e44da85eef9f747b91fadc06c
James Finstrom <jfinstrom@sangoma.com>
2016-02-16 09:49:29 -0700

    13 cleanup, bootstrap tables, floating menu

--- functions.inc.php
+++ functions.inc.php
@@ -97,11 +97,7 @@ function pinsets_list_usage($dispname=true) {
 
 //get the existing meetme extensions
 function pinsets_list() {
-	$results = sql("SELECT * FROM pinsets","getAll",DB_FETCHMODE_ASSOC);
-	if(is_array($results)){
-		return $results;
-	}
-	return null;
+	return \FreePBX::Pinsets()->listPinsets();
 }
 
 function pinsets_get($id){
