b787b36e0fa9359e0751286337359259e453b220
nthumann <me@n-thumann.de>
2019-08-17 21:45:19 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Pinsets;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		if (!empty($configs)) {
 				foreach ($configs as $pinset) {
